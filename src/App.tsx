b49ef30ece49e1b9e30c831882465a8099608032
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-07 18:03:17 -0500

    Initialized Tauri project with react and tailwind

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Board from "./components/board";
+
+function App() {
+  return (
+    <main className="bg-zinc-900 h-svh w-svw flex justify-center items-center">
+      <Board />
+    </main>
+  );
+}
+
+export default App;
