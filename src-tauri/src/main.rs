49b9e9da87e4b618d105e743a8161045780466a1
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-08 21:01:48 -0500

    Added allow(unused) to get rid of linting suggestions while working on it and not using the functions yet. Will remove in the future

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+// TODO: Remove and check that everything is being used properly
+#![allow(unused)]
+
+mod board;
 
 fn main() {
     chess_lib::run()
