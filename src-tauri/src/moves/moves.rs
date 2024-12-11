083734435f6c4002af1b2c38a2031fa0e60b26fe
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-11 09:43:12 -0500

    Added ActiveColor and comments about what the structure represents

--- src-tauri/src/moves/moves.rs
+++ src-tauri/src/moves/moves.rs
@@ -1,3 +1,5 @@
+use crate::board::board::ActiveColor;
+
 enum PromotionType {
   Queen,
   Rook,
@@ -23,10 +25,17 @@ enum Piece {
 }
 
 struct Move {
+  // Starting square. Stored as a u8 between 0-63 inclusive. Bitboard operations are easy if we know which square
   from_square: u8,
+  // Ending square. Same as above. Stored as u8 between 0-63 inclusive
   to_square: u8,
+  // What piece type was moved
   piece_moved: Piece,
+  // Which color was active at the time of the move. Represents who made the move
+  active_color: ActiveColor,
+  // Keeps track of whether a pawn promoted and to what piece
   promotion: Option<PromotionType>,
+  // Keeps track of whether a piece was captured and what piece of so
   captured_piece: Option<CapturedPiece>
 }
 
